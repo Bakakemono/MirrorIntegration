f4ddcbad72b8a4eaccc1e4e06b516757f95f82c5
Bakakemono <32707953+Bakakemono@users.noreply.github.com>
2024-09-02 16:46:40 +0200

    Activate player model in in right scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,27 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : NetworkBehaviour {
 
+    [SerializeField] GameObject playerModel;
+
     public override void OnStartLocalPlayer() {
         base.OnStartLocalPlayer();
     }
+
+    private void Start() {
+        playerModel.SetActive(false);
+    }
+
     private void Update() {
+        if(SceneManager.GetActiveScene().name == "Game") {
+            if(!playerModel.activeSelf) {
+                playerModel.SetActive(true);
+            }
+
+            Movement();
+        }
+    }
+
+    private void Movement() {
         if(!isLocalPlayer)
             return;
 
@@ -20,5 +37,6 @@ public class PlayerController : NetworkBehaviour {
         Vector3 playerMovement = new Vector3(h, 0, v) * 0.5f;
 
         transform.position += playerMovement;
+
     }
 }
